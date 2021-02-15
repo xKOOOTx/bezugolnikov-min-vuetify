6c6433a80df308d597950e65880b13eb5ea94e00
Alex <69161083+xKOOOTx@users.noreply.github.com>
2021-02-15 18:57:30 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,4 @@
-<template>
+\<template>
   <v-main>
     <v-container class="">
       <v-row class="h1_wrapper">
@@ -6,38 +6,33 @@
           <h1 class="gallery-header">Enjoy your choice</h1>
         </v-col>
       </v-row>
-      <vue-image-wall :link-images="images"></vue-image-wall>
-<!--      <v-row>-->
-<!--        <v-col-->
-<!--          class="d-flex child-flex col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 col-xs-12  "-->
-<!--          v-for="(image, idx) in imagesIndex"-->
-<!--          :key="idx">-->
-<!--          <div class="imgWrapper">-->
-<!--            <router-link :to="image.route" >-->
-<!--              <v-img-->
-<!--                class="customImg"-->
-<!--                min-height="250px"-->
-<!--                :alt="image.alt"-->
-<!--                :src="image.path"-->
-<!--                aspect-ratio="2"-->
-<!--                >-->
-<!--                  <template v-slot:placeholder>-->
-<!--                    <v-row-->
-<!--                      class="fill-height ma-0"-->
-<!--                      align="center"-->
-<!--                      justify="center"-->
-<!--                    >-->
-<!--                      <v-progress-circular-->
-<!--                        indeterminate-->
-<!--                        color="grey lighten-5"-->
-<!--                      ></v-progress-circular>-->
-<!--                    </v-row>-->
-<!--                  </template>-->
-<!--                </v-img>-->
-<!--            </router-link>-->
-<!--          </div>-->
-<!--        </v-col>-->
-<!--      </v-row>-->
+      <v-row>-->
+          <div class="imgWrapper">-->
+            <router-link :to="image.route" >-->
+              <v-img-->
+                class="customImg"
+                min-height="250px"
+                :alt="image.alt"
+                :src="image.path"
+                aspect-ratio="2"
+                >
+                  <template v-slot:placeholder>
+                    <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
+                    >
+                      <v-progress-circular
+                        indeterminate
+                        color="grey lighten-5"
+                      ></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
+            </router-link>
+          </div>
+        </v-col>
+      </v-row>
     </v-container>
   </v-main>
 </template>
