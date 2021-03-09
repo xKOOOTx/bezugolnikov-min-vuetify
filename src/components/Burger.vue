32a8df848ca5954cc25f5c10f05fcfc05634f1e8
xKOOOTx <kooot123@gmail.com>
2021-03-09 16:02:41 +0300

    commit

--- src/components/Burger.vue
+++ src/components/Burger.vue
@@ -1,6 +1,15 @@
 <template>
-  <div id="burger">
-
+  <div id="burger"
+    :class="{ 'active' : isBurgerActive }"
+    @click.prevent="toggle"
+  >
+    <slot>
+      <button type="button" class="burger-button" title="Menu">
+        <span class="burger-bar burger-bar--1"></span>
+        <span class="burger-bar burger-bar--2"></span>
+        <span class="burger-bar burger-bar--3"></span>
+      </button>
+    </slot>
   </div>
 </template>
 
